110c1e55d35a9c5d517b1d867d76a61e4e7b82e8
gorushkin <gorushkin@gmail.com>
2020-10-02 19:50:10 +0300

    added formik to form

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Channels from './components/Channels';
+import { configureStore } from '@reduxjs/toolkit';
 import App from './components/App';
 
 export default (gon) => {
+  // const store = configureStore({
+  //   reducer: rootReduser,
+  // });
+
   const root = document.getElementById('chat');
   ReactDOM.render(<App gon={gon} />, root);
 };
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
+import { useFormik } from 'formik';
 
-export default () => (
-  <div className="mt-auto">
-    <form novalidate="" className="" _lpchecked="1">
-      <div className="form-group">
-        <div className="input-group">
-          <input name="body" aria-label="body" className="mr-2 form-control" value="" />
-          <button aria-label="submit" type="submit" className="btn btn-primary">
-            Submit
-          </button>
-          <div className="d-block invalid-feedback">&nbsp;</div>
+const InputForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      body: '',
+    },
+
+    onSubmit: (values) => {
+      console.log(values.body);
+    },
+  });
+
+  return (
+    <div className="mt-auto">
+      <form onSubmit={formik.handleSubmit} noValidate="" className="" _lpchecked="1">
+        <div className="form-group">
+          <div className="input-group">
+            <input
+              id="message"
+              onChange={formik.handleChange}
+              name="body"
+              aria-label="body"
+              className="mr-2 form-control"
+              value={formik.values.body}
+            />
+            <button aria-label="submit" type="submit" className="btn btn-primary">
+              Submit
+            </button>
+            <div className="d-block invalid-feedback">&nbsp;</div>
+          </div>
         </div>
-      </div>
-    </form>
-  </div>
-);
+      </form>
+    </div>
+  );
+};
+
+export default InputForm;
